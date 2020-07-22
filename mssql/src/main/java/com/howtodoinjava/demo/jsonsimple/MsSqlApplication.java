50eb718f8ac0928450d5913c3d4b9f7d3e7bf644
dbshack2hire3 <hack2hire03@hack2hire.net>
2020-07-22 18:27:35 +0800

    Final commit

---
+++ mssql/src/main/java/com/howtodoinjava/demo/jsonsimple/MsSqlApplication.java
@@ -0,0 +1,13 @@
+package com.howtodoinjava.demo.jsonsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSqlApplication.class, args);
+	}
+
+}
---
+++ mssql/src/main/java/com/howtodoinjava/demo/jsonsimple/Rest.java
@@ -0,0 +1,100 @@
+package com.howtodoinjava.demo.jsonsimple;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Rest {
+
+	@GetMapping("/selectQuery")
+	public List<String> selectValue() {
+		String connectionUrl = "jdbc:sqlserver://acciontest.cyfl1ri6kafq.us-east-1.rds.amazonaws.com;"
+				+ "databaseName=sample;user=admin;password=admin123";
+		List<String> names = new ArrayList<>();
+		try {
+			System.out.print("Connecting to SQL Server ... ");
+			try (Connection connection = DriverManager.getConnection(connectionUrl)) {
+				System.out.println("Done.");
+				Statement stmt = connection.createStatement();
+				ResultSet rs;
+
+				rs = stmt.executeQuery("SELECT * FROM sample.dbo.emp");
+				while (rs.next()) {
+					names.add( rs.getString(2));
+				}
+			}
+		} catch (Exception e) {
+			System.out.println();
+			e.printStackTrace();
+		}
+		return names;
+	}
+	
+
+
+	@GetMapping("/insertQuery/{name}")
+	public List<String> insertValue(@PathVariable("name") String name) {
+		String connectionUrl = "jdbc:sqlserver://acciontest.cyfl1ri6kafq.us-east-1.rds.amazonaws.com;"
+				+ "databaseName=sample;user=admin;password=admin123";
+
+		try {
+		    System.out.print("Connecting to SQL Server ... ");
+		    try (Connection connection = DriverManager.getConnection(connectionUrl))        {
+		        System.out.println("Done.");
+
+		        String insertSql = "INSERT INTO sample.dbo.emp VALUES(14,'"+name+"',getdate())";
+		        ResultSet resultSet = null;
+		        PreparedStatement prepsInsertProduct = connection.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);
+
+		            prepsInsertProduct.execute();
+		            // Retrieve the generated key from the insert.
+		            resultSet = prepsInsertProduct.getGeneratedKeys();
+
+		            // Print the ID of the inserted row.
+		            while (resultSet.next()) {
+		                System.out.println("Generated: " + resultSet.getString(1));
+		            }
+		    }
+		} catch (Exception e) {
+		    System.out.println();
+		    e.printStackTrace();
+		}
+		return selectValue();
+	}
+	
+	@GetMapping("/spQuery/{name}")
+	public List<String> spQuery(@PathVariable("name") String name) {
+		String connectionUrl = "jdbc:sqlserver://acciontest.cyfl1ri6kafq.us-east-1.rds.amazonaws.com;"
+				+ "databaseName=sample;user=admin;password=admin123";
+
+		try {
+		    System.out.print("Connecting to SQL Server ... ");
+		    try (Connection connection = DriverManager.getConnection(connectionUrl))        {
+		        System.out.println("Done.");
+		        String SQL = "{call sp_insertdata  (?,?)}";
+
+		        CallableStatement cs = connection.prepareCall(SQL);
+
+		        cs.setString(1, "20");
+
+		        cs.setString(2, name);
+		        cs.execute();
+		      }
+		} catch (Exception e) {
+		    System.out.println();
+		    e.printStackTrace();
+		}
+		return selectValue();
+	}
+
+}
